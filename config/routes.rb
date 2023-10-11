a8ea3eacda8f8a37f00f7cef50257af2fe4a360c
kaya <kaaya.xx.x@gmail.com>
2023-10-11 18:15:54 +0900

    ログインシステム・サインインとログアウト実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  get 'sessions/new'
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
