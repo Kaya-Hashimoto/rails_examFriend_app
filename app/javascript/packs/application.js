70f93150f22a0ba430567a75625faac4378e780c
kaya <kaaya.xx.x@gmail.com>
2023-10-13 20:14:27 +0900

    herokuデプロイエラー修正3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,6 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap";
-import "../stylesheets/application";
 
 Rails.start()
 ActiveStorage.start()
