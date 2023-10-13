96afef58dde56c0a2af048b22605290dc3c4846f
kaya <kaaya.xx.x@gmail.com>
2023-10-13 11:06:22 +0900

    画像投稿の確認画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :pictures
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-
 end
