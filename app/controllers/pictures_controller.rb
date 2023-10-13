82d8fc1440926f2becb70721be5d2df2f93dc481
kaya <kaaya.xx.x@gmail.com>
2023-10-13 16:56:54 +0900

    Bootstrapでデザイン作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -40,7 +40,7 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully updated." }
+        format.html { redirect_to picture_url(@picture), notice: "編集しました" }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class PicturesController < ApplicationController
     @picture.destroy
 
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
+      format.html { redirect_to pictures_url, notice: "削除しました" }
       format.json { head :no_content }
     end
   end
